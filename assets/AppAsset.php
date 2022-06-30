420d6189c8fd7003470a46b4809307f9e8a9f601
Charles <charlestang@foxmail.com>
2022-06-30 12:22:28 +0800

    删除无效的方法。

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -22,26 +22,6 @@ class AppAsset extends AssetBundle
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
-        //'kuainiu\accesslog\assets\AdminLteAsset'
     ];
 
-    /**
-     * 按需加载JS方法
-     * @param View $view
-     * @param      $js_url
-     */
-    public static function addScript($view, $js_url)
-    {
-        $view->registerJsFile($js_url, [self::className(), 'depends' => 'neek\acclog\assets\AppAsset']);
-    }
-
-    /**
-     * 按需加载css方法
-     * @param View $view
-     * @param      $css_url
-     */
-    public static function addCss($view, $css_url)
-    {
-        $view->registerCssFile($css_url, [self::className(), 'depends' => 'neek\acclog\assets\AppAsset']);
-    }
 }
